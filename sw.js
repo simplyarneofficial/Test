0d9f13f8126a46eeaaf1b9d77ca1ca004b88a346
Arne <arne.benzin@icloud.com>
2026-07-22 19:49:56 +0200

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE='moped-navigator-shell-4';
-const FILES=['./','./index.html','./assets/css/app.css','./assets/js/app.js','./assets/icons/D14049FE-06CA-4642-82EB-8F91954CB549.png','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
+const CACHE='moped-navigator-v6';
+const CORE=['./','./index.html','./manifest.webmanifest','./assets/css/app.css','./assets/js/app.js'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});
